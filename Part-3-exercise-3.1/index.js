5774f2f54584f8588c8f9cc08fc60df2f3b41834
Pragati Poudel <pragati.poudel37@gmail.com>
2023-01-17 16:23:03 -0500

    Add 3-5

--- Part-3-exercise-3.1/index.js
+++ Part-3-exercise-3.1/index.js
@@ -59,6 +59,30 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
 })
 
+const generateId = () => {
+    const id = Math.random() * 10000;
+    return id;
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    };
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(person);
+
+    response.json(person);
+})
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
