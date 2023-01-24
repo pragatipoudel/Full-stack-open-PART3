a59b3fd3c67756e8c2a5a94ec824ca753a8361f6
Pragati Poudel <pragati.poudel37@gmail.com>
2023-01-24 11:14:52 -0500

    Add 3-16

--- part3-backend/index.js
+++ part3-backend/index.js
@@ -67,13 +67,19 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204).end()
@@ -81,12 +87,7 @@ app.delete('/api/persons/:id', (request, response) => {
         .catch(error => next(error))
 })
 
-const generateId = () => {
-    const id = Math.random() * 10000;
-    return id;
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
 
     if (!body.name || !body.number) {
@@ -106,10 +107,24 @@ app.post('/api/persons', (request, response) => {
         number: body.number
 
     })
-    person.save().then(savedPerson => {
-        response.json(person);
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(person);
+        })
+        .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if(error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'}) 
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
