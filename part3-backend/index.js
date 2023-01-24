5df52f5942e471abaea46220033d5e11f06e87ea
Pragati Poudel <pragati.poudel37@gmail.com>
2023-01-24 12:38:52 -0500

    Add 3-19

--- part3-backend/index.js
+++ part3-backend/index.js
@@ -68,18 +68,18 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-
-    Person.findByIdAndUpdate(request.params.id, person, { new: true})
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+    const { name, number } = request.body
+    
+    Person.findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
+    .then(udatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+    
 })
 
 app.post('/api/persons', (request, response, next) => {
@@ -90,12 +90,6 @@ app.post('/api/persons', (request, response, next) => {
             error: 'Name or number missing'
         })
     };
-
-    if (persons.some(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'Name must be unique'
-        })
-    }
 
     const person = new Person({
         name: body.name,
@@ -114,6 +108,8 @@ const errorHandler = (error, request, response, next) => {
 
     if(error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id'}) 
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
